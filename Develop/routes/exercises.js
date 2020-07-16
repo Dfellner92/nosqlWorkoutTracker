6af606a4b24c4c786b53e0f64d8835e4a5166e1a
Dan Fellner <dfellner92@gmail.com>
2020-07-15 20:04:23 -0400

    updated routes export

--- Develop/routes/exercises.js
+++ Develop/routes/exercises.js
@@ -1,23 +1,21 @@
 const Workout = require("../models/workout.js");
-module.exports = function (app) {
+const router = require("express").Router();
 
-  app.get("/api/workouts", (req, res) => {
-    Workout.find()
-      .then(data => {
-        res.json(data);
-      })
+router.get("/api/workouts", (req, res) => {
+  Workout.find()
+      .then(data => res.json(data))
       .catch(err => {
-        res.status(400).json(err);
+        res.json(err);
       })
-  });
+});
 
-  app.post("/api/workouts", function (req, res) {
-    Workout.create({})
+router.post("/api/workouts", function (req, res) {
+  Workout.create({})
       .then(data => res.json(data))
       .catch(err => {
         console.log("err", err)
         res.json(err);
       })
-  });
+});
 
-}
+module.exports = router;
